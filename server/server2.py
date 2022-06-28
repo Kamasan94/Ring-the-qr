886e9a25f7add7107af3346318238004c2b99c7e
Kamasan94 <pooldaimon94@gmail.com>
2022-06-28 15:29:33 +0200

    prove git

--- server/server2.py
+++ server/server2.py
@@ -3,11 +3,11 @@ import socket
 print("inizio")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("192.168.1.7",8000))
+s.bind(("192.168.1.15",8000))
 s.listen()
 
-def start() :
 
+try:
     while True:
         # now our endpoint knows about the OTHER endpoint.
         clientsocket, address = s.accept()
@@ -15,5 +15,7 @@ def start() :
         clientsocket.send(bytes("Hey there!!!","utf-8"))
         data = clientsocket.recv(1024).decode()
         print(data)
+except KeyboardInterrupt:
+    print('interrupted!')
+    exit(0)
         
-start()
